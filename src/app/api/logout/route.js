a5d9bd5028444ebb0b7d4f17153f224bd168fa54
Taimoor Ali <taimoorali4214@gmail.com>
2024-01-29 18:47:01 +0500

    .

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -3,7 +3,8 @@ import { NextResponse } from "next/server";
 export async function GET() {
 	try {
 		const response = NextResponse.json({ msg: "logout success" }, { status: 200 });
-		response.cookies.delete('accessToken');
+		response.cookies.set("accessToken", "", { httpOnly: true, expires: new Date() });
+		// response.cookies.set("accessToken", "", { httpOnly: true, expires: new Date(0) });
 		return response;
 	} catch (error) {
 		return NextResponse.json({ msg: "error logging out", error }, { status: 500 });
