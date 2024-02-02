1159b1402508864a8651c93690328a7f690dc4f4
Taimoor Ali <taimoorali4214@gmail.com>
2024-02-02 20:21:44 +0500

    try force dynamic

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
-
+export const dynamic = 'force-dynamic'
 export async function GET() {
 	try {
 		const response = NextResponse.json({ msg: "logout success" }, { status: 200 });
 		response.cookies.set("accessToken", "", { httpOnly: true, expires: new Date() });
-		// response.cookies.set("accessToken", "", { httpOnly: true, expires: new Date(0) });
 		return response;
 	} catch (error) {
 		return NextResponse.json({ msg: "error logging out", error }, { status: 500 });
